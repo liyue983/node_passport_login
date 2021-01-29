3a89c84cb828d9501cd4f88f757d6756bad6a1a3
liyue983 <57828995+liyue983@users.noreply.github.com>
2021-01-29 19:17:03 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,4 @@
-// dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
-
-dbPassword = 'mongodb+srv://Yue:liyue54983@mdb0.o2k84.mongodb.net/MDB0?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
 module.exports = {
     mongoURI: dbPassword
